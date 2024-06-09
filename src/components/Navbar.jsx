a74fc2b85e179cc4e9b3af0c4eeda3dd983c208f
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-06-10 00:14:40 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,13 +29,13 @@ function Navbar() {
           <div>
             <img
               src="/images/logo.png"
-              className="w-full max-w-[11rem] lg:max-w-[5rem] mx-auto"
+              className="w-full max-w-[4rem] lg:max-w-[5rem] mx-auto"
               alt=""
             />
           </div>
 
           <button
-            className="text-2xl text-black flex lg:hidden"
+            className="text-2xl text-white flex lg:hidden"
             onClick={() => setSidebarOpen(true)}
           >
             <IoMenu />
@@ -61,28 +61,28 @@ function Navbar() {
       </nav>
 
       <div
-        className={`fixed top-0 right-0 w-[16rem] h-screen z-[1000] bg-secondary border-l-4 border-primary pt-6 px-8 transition-all duration-300 ${
+        className={`fixed top-0 right-0 w-[16rem] h-screen z-[1000] bg-black text-white border-l-4 border-white pt-6 px-8 transition-all duration-300 ${
           !sidebarOpen ? "translate-x-[16rem]" : "translate-x-0"
         }`}
       >
         <div className="flex items-center justify-between mb-8">
-          <img src="/images/logo.png" className="w-full max-w-[9rem]" alt="" />
+          <img src="/images/logo.png" className="w-full max-w-[5rem]" alt="" />
 
           <button
-            className="text-black text-2xl"
+            className="text-white text-2xl"
             onClick={() => setSidebarOpen(false)}
           >
             <IoClose />
           </button>
         </div>
 
-        <div className="space-y-5 flex flex-col text-left items-start text-black font-medium">
+        <div className="space-y-5 flex flex-col text-left items-start text-white font-medium">
           <Link
             to="contact"
             onClick={() => setSidebarOpen(false)}
             className="cursor-pointer text-sm xl:text-base"
           >
-            Contact Us
+            Telegarm
           </Link>
           <Link
             to="roadmap"
@@ -96,7 +96,7 @@ function Navbar() {
             onClick={() => setSidebarOpen(false)}
             className="cursor-pointer text-sm xl:text-base"
           >
-            About Us
+           Twitter
           </Link>
           <Link
             to="tokenomics"
